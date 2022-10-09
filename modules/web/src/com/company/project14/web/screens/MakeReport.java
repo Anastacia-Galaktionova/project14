87b67540458ec41bb9a58c713bf6a704f0c69480
anastacia <anastacia_gal@mail.ru>
2022-10-09 15:18:08 +0300

    Добавлен скрин MakeReport:  Split Panel  LookUp Field - выбор типа отчета

---
+++ modules/web/src/com/company/project14/web/screens/MakeReport.java
@@ -0,0 +1,14 @@
+package com.company.project14.web.screens;
+
+import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+import javax.inject.Inject;
+
+@UiController("project14_MakeReport")
+@UiDescriptor("makeReport.xml")
+@LoadDataBeforeShow
+
+public class MakeReport extends Screen {
+}
